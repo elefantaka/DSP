ab352b11584382bc1c395c5fc77f21670d9588f3
elefantaka <reniawiatr@gmail.com>
2017-05-07 20:15:03 +0200

    10th week

--- app/src/main/java/pl/sincos/calculator/second.java
+++ app/src/main/java/pl/sincos/calculator/second.java
@@ -2,12 +2,24 @@ package pl.sincos.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.VideoView;
 
-public class second extends AppCompatActivity {
+public class second extends AppCompatActivity
+{
+    private Button play;
+    private VideoView video;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        videofun();
+    }
+    private void videofun()
+    {
+        play = (Button) findViewById(R.id.play);
+        video = (VideoView) findViewById(R.id.video);
     }
 }
